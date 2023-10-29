1bfdcb206d16c1dc65eae7975a227461f73a8aea
jliottard <59571132+jliottard@users.noreply.github.com>
2023-10-29 20:00:26 +0100

    add flags n and c

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::io;
 use std::fs;
-use clap::{arg, command, Arg, ArgAction};
+use clap::builder::ValueParserFactory;
+use clap::{arg, command, Arg, ArgAction, value_parser};
 
 fn main() {
     let matches = command!()
@@ -10,14 +11,16 @@ fn main() {
         )
         .arg(
             arg!(
-                -n --number<NUMBER> "print the first N lines per line"
+                -n --number <N> "print the first N lines of the text"
             )
+            .value_parser(value_parser!(u32))
             .required(false)
         )
         .arg(
             arg!(
-                -c --count <NUMBER> "print the first N lines of the file"
+                -c --count <N> "print the first N bytes of the lines"
             )
+            .value_parser(value_parser!(u32))
             .required(false)
         )
         .get_matches();
@@ -31,10 +34,30 @@ fn main() {
     if file_paths.len() > 0 {
         // read the file given by the file path argument
         for file_path in file_paths {
-            contents.push_str(
-                &fs::read_to_string(file_path)
-                    .expect("Should have been able to read the file\n")
-            );
+            let text = &fs::read_to_string(file_path)
+                .expect("Should have been able to read the file\n");
+            if let Some(bytes_count) = matches.get_one::<u32>("count") {
+                if *bytes_count <= text.len().try_into().unwrap() {
+                    // contents.push_str(&contents[..*bytes_count as usize]);
+                    contents = text[..*bytes_count as usize].to_string();
+                }
+            } else {
+                let lines_number = matches.get_one::<u32>("number");
+                let mut line_index = 0;
+                for line in text.lines() {
+                    match lines_number {
+                        Some(end_line) => {
+                            if &line_index == end_line {
+                                break;
+                            }
+                        },
+                        None => (),
+                    }
+                    contents.push_str(line);
+                    contents.push('\n');
+                    line_index = line_index + 1;
+                }
+            }
         }
     } else {
         // read the standard input as text input
